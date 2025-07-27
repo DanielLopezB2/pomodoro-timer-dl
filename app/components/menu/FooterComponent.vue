cd4a8d0f22b792d02f9ff6324a35a379b51fdfa7
Daniel Lopez <dalobe56@gmail.com>
2025-07-27 16:32:50 -0500

    fix: adds responsive design to menu components

--- app/components/menu/FooterComponent.vue
+++ app/components/menu/FooterComponent.vue
@@ -1,6 +1,8 @@
 <template>
-  <footer class="px-6 py-4 text-center">
-    <h1 class="font-black text-sm">Pomodoro Timer by Daniel Lopez</h1>
-    <span class="text-xs">2025</span>
+  <footer class="px-4 py-6 text-center">
+    <h1 class="font-black text-xs sm:text-sm md:text-base lg:text-lg">
+      Pomodoro Timer by Daniel Lopez
+    </h1>
+    <span class="text-[10px] sm:text-xs md:text-sm">2025</span>
   </footer>
 </template>
--- app/components/menu/HeaderComponent.vue
+++ app/components/menu/HeaderComponent.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="px-6 bg-primary-400 text-secondary-950 py-4 text-center shadow-md">
-    <h2 class="text-2xl font-bold italic">Focus on what matters, one block at a time</h2>
+  <div class="px-4 sm:px-6 py-4 sm:py-6 bg-primary-400 text-secondary-950 text-center shadow-md">
+    <h2
+      class="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold italic leading-snug sm:leading-normal"
+    >
+      Focus on what matters, one block at a time
+    </h2>
   </div>
 </template>
-
-<script setup lang="ts"></script>
